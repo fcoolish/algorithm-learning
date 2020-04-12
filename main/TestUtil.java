0bd60bc0bdbc4a71aeaaf5d4280e2d855566a25e
fcoolish <fcoolish@qq.com>
2020-04-12 21:50:34 +0800

    update 0019

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -270,6 +270,45 @@ public class TestUtil {
         Mirror(root.right);
     }
 
+    public ArrayList<Integer> printMatrix(int [][] matrix) {
+        ArrayList<Integer>list = new ArrayList<Integer>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return list;
+        }
+        int up=0,down = matrix.length-1,left = 0,right = matrix[0].length-1;
+        while (true){
+            for(int col = left;col<=right;col++){
+                list.add(matrix[up][col]);
+            }
+            up++;
+            if(up > down){
+                break;
+            }
+            for(int row=up;row<=down;row++){
+                list.add(matrix[row][right]);
+            }
+            right--;
+            if(left > right){
+                break;
+            }
+            for(int col = right;col>=left;col--){
+                list.add(matrix[down][col]);
+            }
+            down--;
+            if(up > down){
+                break;
+            }
+            for(int row=down;row>=up;row--){
+                list.add(matrix[row][left]);
+            }
+            left++;
+            if(left > right){
+                break;
+            }
+
+        }
+        return list;
+    }
 }
 
 
