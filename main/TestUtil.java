2ddff9fb32911c774d33953712d7b7304c4b78ac
fcoolish <fcoolish@qq.com>
2020-04-14 00:48:05 +0800

    update 022

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -360,9 +362,23 @@ public class TestUtil {
         return stack.isEmpty();
     }
 
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(root == null)return  result;
+        Queue<TreeNode>queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            result.add(temp.val);
+            if (temp.left != null)queue.offer(temp.left);
+            if (temp.right!= null)queue.offer(temp.right);
+        }
+        return  result;
+    }
+
     public static void main(String[] args) {
 
-        //LocalDate
+
 
     }
 
