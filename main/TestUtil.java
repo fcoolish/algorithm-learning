8233373ac3b8525e9a6fad68dca9683e2a4b2619
fcoolish <fcoolish@qq.com>
2020-04-14 22:01:53 +0800

    update 023

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -376,6 +376,31 @@ public class TestUtil {
         return  result;
     }
 
+    public boolean VerifySquenceOfBST(int [] sequence) {
+        if(sequence == null || sequence.length == 0){
+            return false;
+        }
+        return helpVerify(sequence,0,sequence.length-1);
+    }
+
+    public boolean helpVerify(int[] sequence,int start,int root){
+        int key = sequence[root];
+        int i;
+        //找到左右子树的分界点
+        for(i=start;i < root;i++) {
+            if (sequence[i] > key) {
+                break;
+            }
+        }
+            //在右子树中判断是否含有小于root的值，如果有返回false
+            for(int j =i;j< root;j++){
+                if(sequence[j] < key){
+                    return false;
+                }
+            }
+        return  helpVerify(sequence,start,i-1)&& helpVerify(sequence,i,root-1);
+    }
+
     public static void main(String[] args) {
 
 
