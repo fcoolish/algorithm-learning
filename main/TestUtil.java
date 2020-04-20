5bbec53c01abb0281356dd6116e59bfdbbc7fcf1
fcoolish <fcoolish@qq.com>
2020-04-21 00:36:13 +0800

    update 029

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -1,7 +1,6 @@
 
 
 
-import javax.xml.transform.Result;
 import java.util.*;
 
 /**
@@ -573,6 +572,39 @@ public class TestUtil {
         return (num > array.length/2)?preValue:0;
     }
 
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(k == 0 || k >input.length)return result;
+        //初次排序，完成k个元素的排序
+        for(int i = 1;i < k;i++){
+            int j = i - 1;
+            int unFindElement = input[i];
+            while(j >=0 && input[j] > unFindElement){
+                input[j+1] = input[j];
+                j--;
+            }
+
+            input[j+1] = unFindElement;
+        }
+
+        //遍历后面的元素，进行k个元素的更新和替换
+        for(int i = k;i < input.length;i++){
+            if(input[i] < input[k-1]){
+                int newK = input[i];
+                int j = k - 1;
+                while(j>=0 && input[j] > newK){
+                    input[j+1] = input[j];
+                    j--;
+                }
+                input[j+1] = newK;
+            }
+        }
+        //把钱k个元素返回
+        for(int i = 0;i < k;i++){
+            result.add(input[i]);
+        }
+        return result;
+    }
 
 
     public static void main(String[] args) {
