a8440f9b0b18066c3b9d59ccbba7570b084d72e7
huazhou <huajiajun@bilibili.com>
2020-05-07 14:45:20 +0800

    update 030

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -606,6 +606,14 @@ public class TestUtil {
         return result;
     }
 
+    public int FindGreatestSumOfSubArray(int[] array) {
+        int max = array[0];
+        for(int i = 1;i < array.length;i++){
+            array[i] += array[i - 1] > 0 ? array[i -1] :0;
+            max = Math.max(max,array[i]);
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
 
