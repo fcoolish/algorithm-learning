eb73e63461cc5d0722ede1e26641212f269233d2
fcoolish <fcoolish@qq.com>
2020-05-07 21:15:07 +0800

    update 030

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -607,6 +607,15 @@ public class TestUtil {
     }
 
 
+    public int FindGreatestSumOfSubArray(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            array[i] += array[i - 1] > 0 ? array[i - 1] : 0;
+            max = Math.max(max, array[i]);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
 
 
