396cf9237bbf3e56e6ec6dbaa4d2d40dd99afcd9
huazhou <huajiajun@bilibili.com>
2020-05-08 14:54:37 +0800

    Revert "update 030"
    This reverts commit a8440f9b

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -606,14 +606,6 @@ public class TestUtil {
         return result;
     }
 
-    public int FindGreatestSumOfSubArray(int[] array) {
-        int max = array[0];
-        for(int i = 1;i < array.length;i++){
-            array[i] += array[i - 1] > 0 ? array[i -1] :0;
-            max = Math.max(max,array[i]);
-        }
-        return max;
-    }
 
     public int FindGreatestSumOfSubArray(int[] array) {
         int max = array[0];
