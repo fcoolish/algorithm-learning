19f6a1d739a9672615102063a51e70d19a5138b1
fcoolish <fcoolish@qq.com>
2020-05-09 21:42:10 +0800

    update 031

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -616,6 +616,20 @@ public class TestUtil {
         return max;
     }
 
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int count = 0;
+        while(n > 0 ){
+            String str  = String.valueOf(n);
+            char[] chars = str.toCharArray();
+            for(int i = 0;i< chars.length;i++){
+                if(chars[i] == '1')
+                    count++;
+            }
+            n--;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
 
 
