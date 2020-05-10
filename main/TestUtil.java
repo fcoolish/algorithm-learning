d33fa489795bd887744641a84edf5e1594965f1b
fcoolish <fcoolish@qq.com>
2020-05-11 01:42:50 +0800

    update 035

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -677,10 +677,47 @@ public class TestUtil {
         }
         return -1;
     }
+
+    private int cnt;
+    private void MergeSort(int[] array,int start,int end){
+        if(start >= end)return;
+        int mid = (start + end )/2;
+        MergeSort(array,start,mid);
+        MergeSort(array,mid+1,end);
+        MergeOne(array,start,mid,end);
+    }
+    private void MergeOne(int[] array,int start,int mid,int end){
+        int[] temp = new int[end - start+1];
+        int k = 0,i = start,j = mid + 1;
+        while(i <=mid && j <=end){
+            if(array[i] <= array[j])
+                temp[k++] = array[i++];
+            else{
+                temp[k++] = array[j++];
+                cnt = (cnt+(mid - i + 1))%1000000007;
+            }
+        }
+        while (i <= mid)
+            temp[k++] = array[i++];
+        while (j <= end)
+            temp[k++] = array[j++];
+        for (int l = 0;l<k;l++){
+            array[start+l] = temp[l];
+        }
+    }
+
+    public int InversePairs(int [] array) {
+        MergeSort(array,0,array.length - 1);
+        return cnt;
+    }
+
+
+
+
     public static void main(String[] args) {
         //String str = "baccss";
         //int i = FirstNotRepeatingChar(str);
-        //System.out.print(i);
+        System.out.print(i);
     }
 
 }
