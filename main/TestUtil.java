aa72352a59458cebe7024321e29ff701377794d9
fcoolish <fcoolish@qq.com>
2020-05-10 16:44:01 +0800

    update 033

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -650,6 +650,20 @@ public class TestUtil {
         return str;
     }
 
+    public int GetUglyNumber_Solution(int index) {
+        if(index <=0)return 0;
+        int p2=0,p3=0,p5=0;
+        int[] result = new int[index];
+        result[0] = 1;
+        for(int i = 1;i < index;i++){
+            result[i] = Math.min(result[p2]*2,Math.min(result[p3]*3,result[p5]*5));
+            if(result[i] == result[p2]*2)p2++;
+            if(result[i] == result[p3]*3)p3++;
+            if(result[i] == result[p5]*5)p5++;
+        }
+        return result[index - 1];
+    }
+
     public static void main(String[] args) {
 
 
