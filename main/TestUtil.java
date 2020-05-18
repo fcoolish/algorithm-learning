61c03469d376598ba40b2717b29b25a4e5b56045
fcoolish <fcoolish@qq.com>
2020-05-19 00:54:57 +0800

    update 045

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -872,6 +872,27 @@ public class TestUtil {
         return  result.toString();
     }
 
+    public boolean isContinuous(int [] numbers) {
+        if(numbers == null || numbers.length != 5){
+            return false;
+        }
+        int num = 0;
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for(int i = 0;i < numbers.length;i++){
+            if(numbers[i] == 0){
+                num++;
+            }else{
+                set.add(numbers[i]);
+            }
+        }
+        if((num + set.size() != 5)){
+            return false;
+        }
+        if((set.last() - set.first()) < 5){
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
 //        TreeNode node1 = new TreeNode(1);
