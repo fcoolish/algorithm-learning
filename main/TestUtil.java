336a75bfcf9d109d23d40b2fe70f897f8a16469a
fcoolish <fcoolish@qq.com>
2020-05-21 01:11:47 +0800

    update 48

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -905,6 +905,18 @@ public class TestUtil {
         return sum;
     }
 
+    public int Add(int num1,int num2) {
+        int result = 0;
+        int carry = 0;
+        do{
+            result = num1 ^ num2;
+            carry = (num1 & num2) << 1;
+            num1 = result;
+            num2 = carry;
+        }while (carry != 0);
+        return result;
+    }
+
     public static void main(String[] args) {
 //        TreeNode node1 = new TreeNode(1);
 //        TreeDepth(node1);
