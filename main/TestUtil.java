08d8fb30b17cebb317ce3e4527a0a32796098737
fcoolish <fcoolish@qq.com>
2020-05-23 21:28:08 +0800

    update 049

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -917,6 +917,38 @@ public class TestUtil {
         return result;
     }
 
+    public int StrToInt(String str) {
+        if(str == null || str.length() == 0)return  0;
+        char[] arr = str.toCharArray();
+        int i = 0;
+        int flag = 1;
+        int res = 0;
+        if(arr[i] == '-'){
+            flag = -1;
+        }
+        if(arr[i] == '+' || arr[i] == '-'){
+            i++;
+        }
+        while(i < arr.length){
+            //是数字
+            if(arr[i] >='0' && arr[i] <='9'){
+                int cur = arr[i] - '0';
+                if(flag == 1 && (res > Integer.MAX_VALUE / 10) || res == Integer.MAX_VALUE/10 && cur >7){
+                    return 0;
+                }
+                if(flag == -1 && (res > Integer.MAX_VALUE / 10) || res == Integer.MAX_VALUE/10 && cur >8){
+                    return 0;
+                }
+                res = res * 10 + cur;
+                i++;
+            }else{
+                //不是数字
+                return 0;
+            }
+        }
+        return res*flag;
+    }
+
     public static void main(String[] args) {
 //        TreeNode node1 = new TreeNode(1);
 //        TreeDepth(node1);
