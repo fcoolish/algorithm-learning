4c96a0e5f9ef7a05ca39a1fcef9a9d63c63b6d80
huazhou <huajiajun@bilibili.com>
2020-05-28 20:15:17 +0800

    update 058

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -1131,6 +1131,25 @@ public class TestUtil {
 		return  null;
 	}
 
+    boolean isSymmetrical(TreeNode pRoot)
+    {
+        if(pRoot == null)return true;
+        return judgeTree(pRoot.left,pRoot.right);
+    }
+
+    public boolean judgeTree(TreeNode left,TreeNode right){
+        if(left == null & right == null){
+            return true;
+        }else if(left == null || right == null){
+            return false;
+        }
+        if(left.val != right.val){
+            return false;
+        }else{
+            return judgeTree(left.left,right.right) &&judgeTree(left.right,right.left);
+        }
+    }
+
 
     public static void main(String[] args) {
 //        TreeNode node1 = new TreeNode(1);
