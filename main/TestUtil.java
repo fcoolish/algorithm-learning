f9b86d7344845600132733d512dd0cadba94af76
huazhou <huajiajun@bilibili.com>
2020-05-29 20:11:00 +0800

    update 059

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -1150,6 +1150,36 @@ public class TestUtil {
         }
     }
 
+    public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+        LinkedList<TreeNode> q = new LinkedList();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        boolean rev  = true;
+        q.add(pRoot);
+        while (!q.isEmpty()){
+            int size = q.size();
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                if(node == null){
+                    continue;
+                }
+                if(rev){
+                    list.add(node.val);
+                }else{
+                    list.add(0,node.val);
+                }
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+            if(list.size() != 0){
+                res.add(list);
+            }
+            rev = !rev;
+        }
+        return res;
+    }
+
+
 
     public static void main(String[] args) {
 //        TreeNode node1 = new TreeNode(1);
