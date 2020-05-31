4b5c9efba1cfdab244550bbec5db3a41ddcf8fed
fcoolish <fcoolish@qq.com>
2020-06-01 00:18:18 +0800

    update 061

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -1201,6 +1201,31 @@ public class TestUtil {
         }
         return res;
     }
+    int index = -1;
+
+    String Serialize(TreeNode root) {
+        if(root == null){
+            return "#";
+        }else{
+            return root.val + ","+ Serialize(root.left) + "," +Serialize(root.right);
+        }
+    }
+    TreeNode Deserialize(String str) {
+        String[] s = str.split(",");
+        index++;
+        int len = s.length;
+        if(index > len){
+            return null;
+        }
+        TreeNode treeNode = null;
+        if(!s[index].equals("#")){
+            treeNode = new TreeNode(Integer.parseInt(s[index]));
+            treeNode.left = Deserialize(str);
+            treeNode.right = Deserialize(str);
+
+        }
+        return treeNode;
+    }
 
     public static void main(String[] args) {
 //        TreeNode node1 = new TreeNode(1);
