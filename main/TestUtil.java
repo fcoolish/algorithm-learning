3830c1fe9f65ed281412413a69a58e387f52ebb9
huazhou <huajiajun@bilibili.com>
2020-06-04 18:23:36 +0800

    update 065

--- main/TestUtil.java
+++ main/TestUtil.java
@@ -1303,6 +1303,33 @@ public class TestUtil {
         return  res;
     }
 
+    boolean[] visited = null;
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str)
+    {
+        visited = new boolean[matrix.length];
+        for(int i = 0;i < rows;i++){
+            for(int j = 0;j< cols;j++){
+                if(subHasPath(matrix,rows,cols,str,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean subHasPath(char[] matrix, int rows, int cols, char[] str,int row,int col,int len){
+        if(matrix[row * cols + col] != str[len] || visited[row *cols + col ] == true)return false;
+        if(len == str.length - 1) return  true;
+        visited[row * cols + col] = true;
+        if(row > 0 && subHasPath(matrix,rows,cols,str,row - 1,col,len + 1)) return  true;
+        if(row < rows - 1 && subHasPath(matrix, rows, cols, str, row + 1, col, len + 1)) return true;
+        if(col > 0 && subHasPath(matrix, rows, cols, str, row, col - 1, len + 1)) return true;
+        if(col < col - 1 && subHasPath(matrix, rows, cols, str, row, col + 1, len + 1)) return true;
+        visited[row * cols +col] = false;
+        return  false;
+
+    }
+
     public static void main(String[] args) {
 //        TreeNode node1 = new TreeNode(1);
 //        TreeDepth(node1);
