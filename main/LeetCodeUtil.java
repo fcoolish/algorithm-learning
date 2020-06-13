c19f183782285ff29de79166781d26fe484a5267
fcoolish <fcoolish@qq.com>
2020-06-13 18:17:03 +0800

    update leetcode001

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -1,13 +1,11 @@
-题目描述
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+import java.util.*;
+
+/**
+ * Created by admin on 2020/6/13.
+ */
+public class LeetCodeUtil {
 
 
-
-
-
-
-public class Solution {
     public int[] twoSum(int[] nums, int target) {
         if(nums == null || nums.length ==0 ){
             return null;
@@ -22,6 +20,8 @@ public class Solution {
         }
         return null;
     }
+
+    public static void main(String[] args) {
+
+    }
 }
-
-
