fe99988af5b0802e606680640212f6d1e8cc49e2
fcoolish <fcoolish@qq.com>
2020-08-02 20:06:19 +0800

    update:lc005

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -101,6 +101,38 @@ public class LeetCodeUtil {
         }
     }
 
+    public String longestPalindrome(String s) {
+        int len =s.length();
+        if(len < 2){
+            return s;
+        }
+        int maxLen = 1,begin = 0;
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+        for(int i =0;i < len;++){
+            dp[i][i] = true;
+        }
+        for(int j = 1;j < len;j++){
+            for(int i = 0;i < j;i++){
+                if(charArray[i] != charArray[j]){
+                    dp[i][j] = false;
+                }else{
+                    if(j - i <3){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                //只要条件成立就是回文
+                if(dp[i][j] && j - i +1 >maxLen){
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return  s.substring(begin,begin +maxLen);
+    }
+
     public static void main(String[] args) {
 
     }
