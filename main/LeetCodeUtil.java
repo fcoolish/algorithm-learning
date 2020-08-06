e9e10b6922b84bbec12f7de8f5dfeecde6bac6bc
fcoolish <fcoolish@qq.com>
2020-08-07 00:20:29 +0800

    update:lc006

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -133,6 +133,26 @@ public class LeetCodeUtil {
         return  s.substring(begin,begin +maxLen);
     }
 
+    public String convert(String s, int numRows) {
+        if(numRows == 1)return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0;i< Math.min(numRows,s.length()); i++){
+            rows.add(new StringBuilder());
+        }
+        int curRow = 0;
+        boolean goingDown = false;
+        for(char c:s.toCharArray()){
+            rows.get(curRow).append(c);
+            if(curRow == 0 ||curRow == numRows - 1)
+                goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+        StringBuilder ret = new StringBuilder();
+        for(StringBuilder row:rows)
+            ret.append(row);
+        return  ret.toString();
+    }
+
     public static void main(String[] args) {
 
     }
