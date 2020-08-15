aac5cb970e392598454504ce371f54882cc17a86
fcoolish <fcoolish@qq.com>
2020-08-15 21:42:41 +0800

    update:lc009

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -198,7 +198,15 @@ public class LeetCodeUtil {
         return negative?-ans:ans;
     }
 
-
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0))return false;
+        int revertedNumber = 0;
+        while(x > revertedNumber){
+            revertedNumber = revertedNumber * 10 + x %10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
 
     public static void main(String[] args) {
         //int x = reverse(1534236469);
