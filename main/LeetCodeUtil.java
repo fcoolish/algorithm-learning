834198875b3f37f494c25b567f3ec010ffbb571e
fcoolish <fcoolish@qq.com>
2020-08-22 23:51:36 +0800

    update:lc010

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -208,8 +208,41 @@ public class LeetCodeUtil {
         return x == revertedNumber || x == revertedNumber / 10;
     }
 
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for(int i = 0;i <= m; i++){
+            for(int j = 1;j <= n; ++j){
+                if(p.charAt(j-1) == '*'){
+                    f[i][j] = f[i][j - 2];
+                    if(matches(s,p,i,j - 1)){
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                }else{
+                    if(matches(s,p,i,j)){
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public boolean matches(String s,String p,int i,int j){
+        if(i == 0){
+            return  false;
+        }
+        if(p.charAt(j - 1) == '.'){
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+
     public static void main(String[] args) {
-        //int x = reverse(1534236469);
-        //System.out.println("res:"+x);
+
+        //System.out.println("list:");
     }
 }
