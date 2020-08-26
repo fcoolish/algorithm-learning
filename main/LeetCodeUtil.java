48f26897a8f7e5db0ada1110097d1e8bca821189
fcoolish <fcoolish@qq.com>
2020-08-26 23:33:38 +0800

    update:lc012

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -256,6 +256,19 @@ public class LeetCodeUtil {
         return ans;
     }
 
+    public String intToRoman(int num) {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i < values.length;i++){
+            while(values[i] <= num){
+                num -=values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
