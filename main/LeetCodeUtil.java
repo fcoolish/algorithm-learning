730555fb77219cc865512bc26f38222fd002a85a
fcoolish <fcoolish@qq.com>
2020-08-27 00:02:32 +0800

    update:lc013

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -269,6 +269,35 @@ public class LeetCodeUtil {
         return sb.toString();
     }
 
+    public int romanToInt(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for(int i = 1;i < s.length();i++){
+            int num = getValue(s.charAt(i));
+            if(num > preNum){
+                sum -= preNum;
+            }else{
+                sum +=preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+
+    public int getValue(char ch){
+        switch (ch){
+            case 'I':return 1;
+            case 'V':return 5;
+            case 'X':return 10;
+            case 'L':return 50;
+            case 'C':return 100;
+            case 'D':return 500;
+            case 'M':return 1000;
+            default:return 0;
+        }
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
