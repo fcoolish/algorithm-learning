78c012d31ac4f6706ce83af7fc00204c4d0fbd4d
fcoolish <fcoolish@qq.com>
2020-08-29 17:19:36 +0800

    update:lc013

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -298,6 +298,23 @@ public class LeetCodeUtil {
         }
     }
 
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return  "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for(int i = 0;i<length;i++){
+            char c = strs[0].charAt(i);
+            for(int j = 1;j<count;j++){
+                if(strs[j].length() ==i || strs[j].charAt(i) != c){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
