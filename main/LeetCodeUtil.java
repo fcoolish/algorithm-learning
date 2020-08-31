f05048bf78b156cd641288233cccedf499771f5c
fcoolish <fcoolish@qq.com>
2020-08-31 23:52:22 +0800

    update:lc015

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -315,6 +315,46 @@ public class LeetCodeUtil {
         return strs[0];
     }
 
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        //枚举a
+        for(int first = 0;first < n;++first){
+            //需要和上一次枚举的数不相同
+            if(first > 0&&nums[first] == nums[first - 1]){
+                continue;
+            }
+            int third = n - 1;
+            int target  = -nums[first];
+            //枚举b
+            for(int second = first + 1;second < n; ++second){
+                //需要和上一次不同
+                if(second > first + 1&& nums[second] == nums[second -1]){
+                    continue;
+                }
+                //需要保证b的指针在c的左侧
+                while(second < third && nums[second] + nums[third] > target){
+                    --third;
+                }
+                //如果指针重复，随着b后续增加
+                //就会有a+b+c=0且b<c的c了，退出循环
+                if(second == third){
+                    break;
+                }
+                if(nums[second] + nums[third] == target){
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
