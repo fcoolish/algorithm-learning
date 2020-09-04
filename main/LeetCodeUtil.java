dc846b4ffede04938d815bcb7552c7e992c66950
fcoolish <fcoolish@qq.com>
2020-09-04 23:39:55 +0800

    update:lc020

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -479,6 +479,33 @@ public class LeetCodeUtil {
         return pHead.next;
     }
 
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n % 2 == 1){
+            return false;
+        }
+
+        Map<Character,Character> pairs = new HashMap<Character, Character>(){{
+            put(')','(');
+            put(']','[');
+            put('}','{');
+        }};
+
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0;i< n;i++){
+            char ch = s.charAt(i);
+            if(pairs.containsKey(ch)){
+                if(stack.isEmpty() || stack.peek() != pairs.get(ch)){
+                    return false;
+                }
+                stack.pop();
+            }else{
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
