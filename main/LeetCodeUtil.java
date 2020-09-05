f65bc16b6d04b9fcbd1368983d9e1f8829bc86cf
fcoolish <fcoolish@qq.com>
2020-09-05 15:04:45 +0800

    update:lc021

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -506,6 +506,24 @@ public class LeetCodeUtil {
         return stack.isEmpty();
     }
 
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while(l1 !=null && l2!= null){
+            if(l1.val >=l2.val){
+                temp.next = l2;
+                l2 = l2.next;
+            }else{
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            temp = temp.next;
+        }
+        if(l1 !=null)temp.next = l1;
+        if(l2 !=null)temp.next = l2;
+        return head.next;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
