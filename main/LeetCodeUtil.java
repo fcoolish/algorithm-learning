4611341d0501b715e22f8bff2f0de1fa814516b2
fcoolish <fcoolish@qq.com>
2020-09-09 23:42:33 +0800

    update:lc024

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -563,6 +563,16 @@ public class LeetCodeUtil {
         }
     }
 
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode first = head;
+        ListNode second  =  head.next;
+        first.next = swapPairs(second.next);
+        second.next = first;
+        return second;
+    }
 
     public static void main(String[] args) {
 
