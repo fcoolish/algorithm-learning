52c4ea8cb320d7419488f69096e875336e4ecc5e
fcoolish <fcoolish@qq.com>
2020-09-10 23:00:49 +0800

    update:lc025

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -574,6 +574,41 @@ public class LeetCodeUtil {
         return second;
     }
 
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+        while(end.next != null){
+            for(int i =0;i<k&&end!=null;i++){
+                end = end.next;
+            }
+            if(end == null){
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return  pre;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
