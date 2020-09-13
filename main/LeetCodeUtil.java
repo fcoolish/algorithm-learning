257aee4c81a3168ecd83f301c87b66c0e391adb0
fcoolish <fcoolish@qq.com>
2020-09-13 23:29:13 +0800

    update:lc026

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -609,6 +609,18 @@ public class LeetCodeUtil {
         return  pre;
     }
 
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length ==0)return 0;
+        int i = 0;
+        for(int j = 1;j < nums.length;j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
