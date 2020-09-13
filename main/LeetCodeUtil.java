ddf66ac1d509c57c090b99a932746c0406d7afbb
fcoolish <fcoolish@qq.com>
2020-09-13 23:45:07 +0800

    update:lc027

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -621,6 +621,19 @@ public class LeetCodeUtil {
         return i + 1;
     }
 
+
+    public int removeElement(int[] nums, int val) {
+        if(nums == null || nums.length ==0)return 0;
+        int i = 0;
+        for(int j = 0;j < nums.length;j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
