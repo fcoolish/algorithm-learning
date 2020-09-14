17cb6cb2d7a7b3b1104f57da8a26626197a051bd
fcoolish <fcoolish@qq.com>
2020-09-15 00:38:02 +0800

    update:lc028

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -634,6 +634,27 @@ public class LeetCodeUtil {
         return i;
     }
 
+    public int strStr(String haystack, String needle) {
+        int n  = haystack.length();
+        int L = needle.length();
+        if(L == 0)return 0;
+        int pn = 0;
+        while(pn < n - L + 1){
+            while(pn < n - L + 1&&haystack.charAt(pn) != needle.charAt(0)){
+                    pn++;
+                }
+                int currLen = 0 ,pL = 0;
+                while (pL < L && pn <n && haystack.charAt(pn) == needle.charAt(pL)){
+                    pn++;
+                    pL++;
+                    currLen++;
+                }
+                if(currLen == L)return pn -L;
+                pn = pn - currLen + 1;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
