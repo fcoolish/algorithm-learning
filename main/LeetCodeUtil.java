24a4e64834c90a9883cba79cf533db05bda280b4
fcoolish <fcoolish@qq.com>
2020-09-23 23:51:26 +0800

    update:lc030

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -687,6 +687,28 @@ public class LeetCodeUtil {
         return count+calDiv(a - tb,b);
     }
 
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(s == null || s.length() == 0 || words == null || words.length == 0)return res;
+        HashMap<String,Integer> map  = new HashMap<String, Integer>();
+        int one_word = words[0].length();
+        int word_num = words.length;
+        int all_len = one_word * word_num;
+        for(String word:words){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+        for(int i = 0;i <s.length() - all_len + 1;i++){
+            String tmp = s.substring(i,i+all_len);
+            HashMap<String,Integer> tmp_map = new HashMap<String, Integer>();
+            for(int j = 0;j < all_len;j +=one_word){
+                String w = tmp.substring(j,j + one_word);
+                tmp_map.put(w,tmp_map.getOrDefault(w,0) + 1);
+            }
+            if(map.equals(tmp_map))res.add(i);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
