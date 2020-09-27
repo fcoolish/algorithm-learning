44c0d11ac2a7cffa0c5aca21e372133c97643deb
fcoolish <fcoolish@qq.com>
2020-09-27 23:42:14 +0800

    update:lc031

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -709,6 +709,36 @@ public class LeetCodeUtil {
         return res;
     }
 
+    public void nextPermutation(int[] nums) {
+        int i = nums.length -2;
+        while(i >=0 && nums[i + 1] <= nums[i]){
+            i--;
+        }
+        if(i >=0){
+            int j = nums.length - 1;
+            while(j >=0 && nums[j] <= nums[i]){
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        reverse(nums,i + 1);
+    }
+
+    public void reverse(int[] nums,int start){
+        int i = start,j = nums.length -1;
+        while(i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
