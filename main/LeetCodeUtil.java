38b58e3c4cef20fbcce05a6d121ac5862bedb22f
fcoolish <fcoolish@qq.com>
2020-10-02 23:47:51 +0800

    update:lc033

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -757,6 +757,33 @@ public class LeetCodeUtil {
         return max;
     }
 
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0)return -1;
+        int n = nums.length;
+        if(n == 1){
+            return nums[0] == target?0:-1;
+        }
+        int l = 0,r = n -1;
+        while(l <=r){
+            int mid = (l + r)/2;
+            if(nums[mid] == target)return mid;
+            if(nums[0] <= nums[mid]){
+                if(nums[0] <= target && target < nums[mid]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target&&target <=nums[n - 1]){
+                    l = mid +1;
+                }else{
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
 
         //System.out.println("list:");
