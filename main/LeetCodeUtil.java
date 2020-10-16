1641e1beac492782893d6af2a76f13b6dc7d9793
fcoolish <fcoolish@qq.com>
2020-10-16 23:54:18 +0800

    update:lc037

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -831,6 +831,42 @@ public class LeetCodeUtil {
         return true;
     }
 
+    boolean[][] line = new boolean[9][9];
+    boolean[][] column = new boolean[9][9];
+    boolean[][][] block = new boolean[3][3][9];
+    boolean valid = false;
+    List<int[]> spaces = new ArrayList<int[]>();
+    public void solveSudoku(char[][] board) {
+        for(int i = 0;i < 9;i++){
+            for(int j = 0;j < 9;j++){
+              if(board[i][j] =='.'){
+                  spaces.add(new int[]{i,j});
+              }else{
+                  int digit = board[i][j] - '0' - 1;
+                  line[i][digit] = column[j][digit] = block[i/3][j/3][digit] = true;
+              }
+            }
+        }
+        dfs(board,0);
+    }
+
+    public void dfs(char[][] board,int pos){
+        if(pos == spaces.size()){
+            valid = true;
+            return;
+        }
+        int[] spece = spaces.get(pos);
+        int i = spece[0],j = spece[1];
+        for(int digit = 0;digit < 9&&!valid;digit++){
+            if(!line[i][digit] && !column[j][digit]&&!block[i/3][j/3][digit]){
+                line[i][digit] = column[j][digit] = block[i/3][j/3][digit] = true;
+                board[i][j] = (char)(digit + '0' + 1);
+                dfs(board,pos + 1);
+                line[i][digit] = column[j][digit] = block[i/3][j/3][digit] = false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{5,7,7,8,8,10};
         //searchRange(array,8);
