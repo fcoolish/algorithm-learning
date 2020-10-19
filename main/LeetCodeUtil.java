365305cd230e9e871b976658ba0029be5f92ec2e
fcoolish <fcoolish@qq.com>
2020-10-19 22:50:39 +0800

    update:lc039

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -884,6 +884,29 @@ public class LeetCodeUtil {
         return res.toString();
     }
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates,target,ans,combine,0);
+        return ans;
+    }
+
+    public void dfs(int[] candidates,int target,List<List<Integer>> ans,List<Integer> combine,int idx){
+        if(idx == candidates.length){
+            return;
+        }
+        if(target == 0){
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        dfs(candidates,target,ans,combine,idx +1);
+        if(target - candidates[idx] >=0){
+            combine.add(candidates[idx]);
+            dfs(candidates,target - candidates[idx],ans,combine,idx);
+            combine.remove(combine.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{5,7,7,8,8,10};
         //searchRange(array,8);
