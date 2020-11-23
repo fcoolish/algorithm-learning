174766a88a771714113ff4ecf966e06a684b95ac
fcoolish <fcoolish@qq.com>
2020-11-23 23:55:17 +0800

    update:lc043

--- main/LeetCodeUtil.java
+++ main/LeetCodeUtil.java
@@ -989,6 +989,34 @@ public class LeetCodeUtil {
         return ans;
     }
 
+    public String multiply(String num1, String num2) {
+        if("0".equals(num1) || "0".equals(num2)){
+            return "0";
+        }
+
+        int m = num1.length(),n = num2.length();
+        int[] ansArr = new int[m + n];
+        for(int i = m -1;i >=0;i--){
+            int x = num1.charAt(i) - '0';
+            for(int j = n - 1;j >=0;j--){
+                int y = num2.charAt(j) - '0';
+                ansArr[i + j + 1] += x * y;
+            }
+
+        }
+        for(int i = m + n - 1;i > 0;i--){
+            ansArr[i - 1] += ansArr[i] / 10;
+            ansArr[i] %= 10;
+        }
+        int index = ansArr[0] == 0 ? 1 : 0;
+        StringBuffer ans = new StringBuffer();
+        while(index < m + n){
+            ans.append(ansArr[index]);
+            index++;
+        }
+        return ans.toString();
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{5,7,7,8,8,10};
         //searchRange(array,8);
