e10d6a234547e9c7ec3e27ae8481468631dd3a76
fcoolish <fcoolish@qq.com>
2022-05-13 19:16:21 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/LianXuZiShuZuDeZuiDaHeLcof.java
@@ -0,0 +1,45 @@
+//输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。 
+//
+// 要求时间复杂度为O(n)。 
+//
+// 
+//
+// 示例1: 
+//
+// 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+//输出: 6
+//解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= arr.length <= 10^5 
+// -100 <= arr[i] <= 100 
+// 
+//
+// 注意：本题与主站 53 题相同：https://leetcode-cn.com/problems/maximum-subarray/ 
+//
+// 
+// Related Topics 数组 分治 动态规划 👍 530 👎 0
+
+package leetcode.editor.cn;
+public class LianXuZiShuZuDeZuiDaHeLcof{
+    public static void main(String[] args){
+        Solution solution = new LianXuZiShuZuDeZuiDaHeLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0,max = nums[0];
+        for(int num:nums){
+           sum = Math.max(sum + num,num);
+           max = Math.max(max,sum);
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
