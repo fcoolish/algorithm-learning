cc6f37de42dc5f52e052b7f36c2eace1e41c7b32
fcoolish <fcoolish@qq.com>
2022-06-15 22:40:37 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/N6YdxV.java
@@ -0,0 +1,93 @@
+//给定一个排序的整数数组 nums 和一个整数目标值 target ，请在数组中找到 target ，并返回其下标。如果目标值不存在于数组中，返回它将会被按顺
+//序插入的位置。 
+//
+// 请必须使用时间复杂度为 O(log n) 的算法。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 5
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 2
+//输出: 1
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 7
+//输出: 4
+// 
+//
+// 示例 4: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 0
+//输出: 0
+// 
+//
+// 示例 5: 
+//
+// 
+//输入: nums = [1], target = 0
+//输出: 0
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums 为无重复元素的升序排列数组 
+// -10⁴ <= target <= 10⁴ 
+// 
+//
+// 
+//
+// 注意：本题与主站 35 题相同： https://leetcode-cn.com/problems/search-insert-position/ 
+// Related Topics 数组 二分查找 👍 25 👎 0
+
+package leetcode.editor.cn;
+public class N6YdxV{
+    public static void main(String[] args){
+        Solution solution = new N6YdxV().new Solution();
+        int[] nums = new int[]{1,3,5,6};
+        solution.searchInsert(nums,0);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length - 1;
+        int l = 0, r= n;
+        int index = 0;
+        while (l <= r){
+            int mid = (l + r) >> 1;
+            if(nums[mid] > target){
+                r = mid - 1;
+                index = r;
+            }else if(nums[mid] < target){
+                l = mid + 1;
+                index = l;
+            }else{
+                return mid;
+            }
+        }
+        if(index > n){
+            return index;
+        }
+        if(index < 0)return 0;
+        return nums[index] > target ? index:index + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
