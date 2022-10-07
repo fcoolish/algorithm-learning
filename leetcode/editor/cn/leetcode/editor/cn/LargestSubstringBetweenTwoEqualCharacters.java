674cbdad55717fc2f7effe1e2fbe0c6bbad3bf7e
fcoolish <fcoolish@qq.com>
2022-10-07 14:33:18 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/LargestSubstringBetweenTwoEqualCharacters.java
@@ -0,0 +1,85 @@
+//给你一个字符串 s，请你返回 两个相同字符之间的最长子字符串的长度 ，计算长度时不含这两个字符。如果不存在这样的子字符串，返回 -1 。 
+//
+// 子字符串 是字符串中的一个连续字符序列。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：s = "aa"
+//输出：0
+//解释：最优的子字符串是两个 'a' 之间的空子字符串。 
+//
+// 示例 2： 
+//
+// 输入：s = "abca"
+//输出：2
+//解释：最优的子字符串是 "bc" 。
+// 
+//
+// 示例 3： 
+//
+// 输入：s = "cbzxy"
+//输出：-1
+//解释：s 中不存在出现出现两次的字符，所以返回 -1 。
+// 
+//
+// 示例 4： 
+//
+// 输入：s = "cabbac"
+//输出：4
+//解释：最优的子字符串是 "abba" ，其他的非最优解包括 "bb" 和 "" 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 300 
+// s 只含小写英文字母 
+// 
+// Related Topics 哈希表 字符串 👍 74 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+public class LargestSubstringBetweenTwoEqualCharacters{
+    public static void main(String[] args){
+        Solution solution = new LargestSubstringBetweenTwoEqualCharacters().new Solution();
+        solution.maxLengthBetweenEqualCharacters("aa");
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int[] arr = new int[26];
+        Arrays.fill(arr,-1);
+        int ans = -1;
+        for(int i = 0;i < s.length();i++){
+            int num = s.charAt(i) - 'a';
+            if(arr[num] < 0){
+                arr[num] = i;
+            }else{
+                ans = Math.max(ans,i - arr[num]- 1);
+            }
+        }
+        return ans;
+    }
+    public int maxLengthBetweenEqualCharacters2(String s) {
+            int ans = -1;
+            for(int i = 0;i < s.length();i++){
+                for(int j =i;j < s.length();j++){
+                    if(s.charAt(i) == s.charAt(j)){
+                        ans = Math.max(ans,j - i - 1);
+                    }
+                }
+            }
+            return ans;
+        }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
