6bdf7edf3c02e1a71a91beee0e0ea30261d21cbb
fcoolish <fcoolish@qq.com>
2022-12-01 22:57:39 +0800

    update:lc

--- leetcode/editor/cn/leetcode/editor/cn/XltzEq.java
+++ leetcode/editor/cn/leetcode/editor/cn/XltzEq.java
@@ -36,10 +36,25 @@ package leetcode.editor.cn;
 public class XltzEq{
     public static void main(String[] args){
         Solution solution = new XltzEq().new Solution();
+        solution.isPalindrome("A man, a plan, a canal: Panama");
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isPalindrome(String s) {
+        int i =0,j = s.length() - 1;
+        while (i < j){
+            while (i< j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while (i< j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++;
+            j--;
+        }
         return true;
     }
 }
