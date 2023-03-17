eaf7accbddfc38ff07e95a932bb7c7a8991454ad
fcoolish <fcoolish@qq.com>
2023-03-18 00:52:46 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/TwoSumIvInputIsABst.java
@@ -0,0 +1,83 @@
+//给定一个二叉搜索树 root 和一个目标结果 k，如果二叉搜索树中存在两个元素且它们的和等于给定的目标结果，则返回 true。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入: root = [5,3,6,2,4,null,7], k = 9
+//输出: true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入: root = [5,3,6,2,4,null,7], k = 28
+//输出: false
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [1, 10⁴]. 
+// -10⁴ <= Node.val <= 10⁴ 
+// 题目数据保证，输入的 root 是一棵 有效 的二叉搜索树 
+// -10⁵ <= k <= 10⁵ 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉搜索树 哈希表 双指针 二叉树 👍 460 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class TwoSumIvInputIsABst{
+    public static void main(String[] args){
+        Solution solution = new TwoSumIvInputIsABst().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null)return false;
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(list.contains(k - node.val)){
+                return true;
+            }
+            list.add(node.val);
+            if(node.left != null){
+                queue.offer(node.left);
+            }
+            if(node.right != null){
+                queue.offer(node.right);
+            }
+        }
+        return false;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
