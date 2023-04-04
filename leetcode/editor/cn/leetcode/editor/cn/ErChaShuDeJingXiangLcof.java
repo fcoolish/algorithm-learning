f368bd3874b3f4e550639e2981c55388a22a766b
fcoolish <fcoolish@qq.com>
2023-04-04 22:53:07 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/ErChaShuDeJingXiangLcof.java
@@ -0,0 +1,88 @@
+//请完成一个函数，输入一个二叉树，该函数输出它的镜像。 
+//
+// 例如输入： 
+//
+// 4 
+// / \ 
+// 2 7 
+// / \ / \ 
+//1 3 6 9 
+//镜像输出： 
+//
+// 4 
+// / \ 
+// 7 2 
+// / \ / \ 
+//9 6 3 1 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/ 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 350 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class ErChaShuDeJingXiangLcof{
+    public static void main(String[] args){
+        Solution solution = new ErChaShuDeJingXiangLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null)return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
+
+    public TreeNode mirrorTree1(TreeNode root) {
+        if(root == null)return root;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if(node.left != null){
+                queue.offer(node.left);
+            }
+            if(node.right != null){
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
