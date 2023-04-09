1a1a2ecaa9e5107d3fe538c2419064cbe6f62315
fcoolish <fcoolish@qq.com>
2023-04-09 16:29:09 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,93 @@
+//给定一个非空二叉树的根节点
+// root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10⁻⁵ 以内的答案可以被接受。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[3.00000,14.50000,11.00000]
+//解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+//因此返回 [3, 14.5, 11] 。
+// 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,15,7]
+//输出：[3.00000,14.50000,11.00000]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 
+//
+// 
+// 树中节点数量在 [1, 10⁴] 范围内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 406 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageOfLevelsInBinaryTree{
+    public static void main(String[] args){
+        Solution solution = new AverageOfLevelsInBinaryTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        if(root == null)return ans;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            double sum = 0;
+            for(int i =0;i < size;i++){
+                TreeNode node = queue.poll();
+                if(node.left !=null){
+                    queue.offer(node.left);
+                }
+                if(node.right !=null){
+                    queue.offer(node.right);
+                }
+                sum += node.val;
+            }
+            ans.add(sum/size);
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- leetcode/editor/cn/leetcode/editor/cn/BinaryTreePaths.java
+++ leetcode/editor/cn/leetcode/editor/cn/BinaryTreePaths.java
@@ -31,51 +31,93 @@ package leetcode.editor.cn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
-public class BinaryTreePaths{
-    public static void main(String[] args){
+public class BinaryTreePaths {
+    public static void main(String[] args) {
         Solution solution = new BinaryTreePaths().new Solution();
+        TreeNode node = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(5);
+        node.left = node2;
+        node.right = node3;
+        node2.right = node4;
+        solution.binaryTreePaths(node);
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> ans = new ArrayList<>();
 
-        getPath(root,new StringBuilder(),ans);
-        return ans;
-    }
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
 
-    public void getPath(TreeNode node,StringBuilder st,List<String> ans){
-        if(node != null){
-            StringBuilder str = new StringBuilder(st);
-            str.append(node.val);
+        public List<String> binaryTreePaths(TreeNode root) {
+            List<String> ans = new ArrayList<>();
+            if(root == null)return ans;
+            Stack<TreeNode> stack = new Stack<>();
+            Stack<String> sPath = new Stack<>();
+            stack.push(root);
+            sPath.push(Integer.toString(root.val));
+            while (!stack.isEmpty()){
+                TreeNode node = stack.pop();
+                String path = sPath.pop();
+                if(node.left == null && node.right == null){
+                    ans.add(path.toString());
+                }else{
+                    if(node.right != null){
+                        stack.push(node.right);
+                        sPath.push(new StringBuilder(path).append("->").append(node.right.val).toString());
+                    }
+                    if(node.left != null){
+                        stack.push(node.left);
+                        sPath.push(new StringBuilder(path).append("->").append(node.left.val).toString());
+                    }
+                }
+            }
+            return ans;
+        }
+
+
+
+        public List<String> binaryTreePaths1(TreeNode root) {
+            List<String> ans = new ArrayList<>();
+            getPath(root, new StringBuilder(), ans);
+            return ans;
+        }
+
+
+        public void getPath(TreeNode node,StringBuilder str,List<String> res){
+            if(node == null){
+                return;
+            }
+            StringBuilder ppth = new StringBuilder(str);
+            if(str.length() != 0){
+                ppth.append("->");
+            }
+            ppth.append(node.val);
             if(node.left == null && node.right == null){
-                ans.add(str.toString());
-                str.deleteCharAt(str.length() - 1);
-            }else {
-                str.append("->");
-                getPath(node.left,str,ans);
-                getPath(node.right,str,ans);
+                res.add(ppth.toString());
+            }else{
+                getPath(node.left,ppth,res);
+                getPath(node.right,ppth,res);
             }
         }
-    }
 
-}
+
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
