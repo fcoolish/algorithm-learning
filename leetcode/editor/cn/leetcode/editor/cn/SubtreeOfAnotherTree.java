f9a9769efc3219767b450a81aba98adf56064f82
fcoolish <fcoolish@qq.com>
2023-04-11 01:09:01 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/SubtreeOfAnotherTree.java
@@ -0,0 +1,84 @@
+//
+// 
+// 给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则
+//，返回 false 。 
+// 
+// 
+//
+// 二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,4,5,1,2], subRoot = [4,1,2]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// root 树上的节点数量范围是 [1, 2000] 
+// subRoot 树上的节点数量范围是 [1, 1000] 
+// -10⁴ <= root.val <= 10⁴ 
+// -10⁴ <= subRoot.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 字符串匹配 哈希函数 👍 897 👎 0
+
+package leetcode.editor.cn;
+public class SubtreeOfAnotherTree{
+    public static void main(String[] args){
+        Solution solution = new SubtreeOfAnotherTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        return dfs(root,subRoot);
+    }
+
+    public boolean dfs(TreeNode s, TreeNode t) {
+        if(s == null){
+            return false;
+        }
+        return check(s,t) || isSubtree(s.left,t) || isSubtree(s.right,t);
+    }
+
+    public boolean check(TreeNode s, TreeNode t) {
+        if(s == null && t == null){
+            return true;
+        }else if(s == null || t == null || s.val != t.val){
+            return false;
+        }
+        return check(s.left,t.left) && check(s.right,t.right);
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
