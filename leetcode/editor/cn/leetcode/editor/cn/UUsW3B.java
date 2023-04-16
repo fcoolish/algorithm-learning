63ef4dfb1e5696a8becf4638e5ca24dcc71f12dd
fcoolish <fcoolish@qq.com>
2023-04-16 23:22:05 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/UUsW3B.java
@@ -0,0 +1,81 @@
+//给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: n = 4, k = 2
+//输出:
+//[
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+//] 
+//
+// 示例 2: 
+//
+// 
+//输入: n = 1, k = 1
+//输出: [[1]] 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= n <= 20 
+// 1 <= k <= n 
+// 
+//
+// 
+//
+// 
+// 注意：本题与主站 77 题相同： https://leetcode-cn.com/problems/combinations/ 
+//
+// Related Topics 数组 回溯 👍 46 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UUsW3B{
+    public static void main(String[] args){
+        Solution solution = new UUsW3B().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> input = new ArrayList<>();
+        for(int i = 1;i <= n;i++){
+            input.add(i);
+        }
+        dfs(n,k,input,new ArrayList<>(),ans,0);
+        return ans;
+    }
+
+    public void dfs(int n,int k,List<Integer> input,List<Integer> list,List<List<Integer>> ans,int index){
+        if(list.size() + n - index + 1 < k){//剪枝
+            return;
+        }
+        if(index == n){
+            if(list.size() == k){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        list.add(input.get(index));
+        dfs(n,k,input,list,ans,index + 1);
+        list.remove(input.get(index));
+        dfs(n,k,input,list,ans,index + 1);
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
