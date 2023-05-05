0b93d5118c3f1c2d0c80aba8523be485f0c5762b
fcoolish <fcoolish@qq.com>
2023-05-06 00:55:49 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/ThreeSum.java
@@ -0,0 +1,96 @@
+//给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != 
+//k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请 
+//
+// 你返回所有和为 0 且不重复的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+//解释：
+//nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+//nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+//nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+//不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+//注意，输出的顺序和三元组的顺序并不重要。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,1]
+//输出：[]
+//解释：唯一可能的三元组和不为 0 。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0,0,0]
+//输出：[[0,0,0]]
+//解释：唯一可能的三元组和为 0 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 3 <= nums.length <= 3000 
+// -10⁵ <= nums[i] <= 10⁵ 
+// 
+//
+// Related Topics 数组 双指针 排序 👍 5915 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.*;
+
+public class ThreeSum{
+    public static void main(String[] args){
+        Solution solution = new ThreeSum().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0;i < n;i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int k = n - 1;
+            int target = -nums[i];
+            for(int j = i + 1;j < n;j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                while (j < k && nums[j] + nums[k] > target){
+                    k--;
+                }
+                if(j == k){
+                    break;
+                }
+                if(nums[j] + nums[k] == target){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i] );
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ leetcode/editor/cn/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -0,0 +1,95 @@
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 10⁴] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 2005 👎 0
+
+package leetcode.editor.cn;
+public class ValidateBinarySearchTree{
+    public static void main(String[] args){
+        Solution solution = new ValidateBinarySearchTree().new Solution();
+        TreeNode n1 = new TreeNode(5);
+        TreeNode n2 = new TreeNode(1);
+        TreeNode n3 = new TreeNode(4);
+        TreeNode n4 = new TreeNode(3);
+        TreeNode n5 = new TreeNode(6);
+        n1.left = n2;
+        n1.right = n3;
+        n3.left = n4;
+        n3.right = n5;
+        solution.isValidBST(n1);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    TreeNode pre = null;
+    boolean ans = true;
+    public boolean isValidBST(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private void dfs(TreeNode node){
+        if(node == null) return;
+        dfs(node.left);
+        if(pre == null){
+            pre = node;
+        }else if(pre.val >= node.val){
+            ans = false;
+            return;
+        }else {
+            pre = node;
+        }
+        dfs(node.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
