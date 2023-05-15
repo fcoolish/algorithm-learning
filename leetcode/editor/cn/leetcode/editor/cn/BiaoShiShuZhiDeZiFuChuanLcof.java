6a0a255ee45c8d695a964a76e7c34ac412621776
fcoolish <fcoolish@qq.com>
2023-05-15 23:26:22 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/BiaoShiShuZhiDeZiFuChuanLcof.java
@@ -0,0 +1,200 @@
+//请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。 
+//
+// 数值（按顺序）可以分成以下几个部分： 
+//
+// 
+// 若干空格 
+// 一个 小数 或者 整数 
+// （可选）一个 'e' 或 'E' ，后面跟着一个 整数 
+// 若干空格 
+// 
+//
+// 小数（按顺序）可以分成以下几个部分： 
+//
+// 
+// （可选）一个符号字符（'+' 或 '-'） 
+// 下述格式之一： 
+// 
+// 至少一位数字，后面跟着一个点 '.' 
+// 至少一位数字，后面跟着一个点 '.' ，后面再跟着至少一位数字 
+// 一个点 '.' ，后面跟着至少一位数字 
+// 
+// 
+//
+// 整数（按顺序）可以分成以下几个部分： 
+//
+// 
+// （可选）一个符号字符（'+' 或 '-'） 
+// 至少一位数字 
+// 
+//
+// 部分数值列举如下： 
+//
+// 
+// ["+100", "5e2", "-123", "3.1416", "-1E-16", "0123"] 
+// 
+//
+// 部分非数值列举如下： 
+//
+// 
+// ["12e", "1a3.14", "1.2.3", "+-5", "12e+5.4"] 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "0"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "e"
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "."
+//输出：false 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "    .1  "
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 20 
+// s 仅含英文字母（大写和小写），数字（0-9），加号 '+' ，减号 '-' ，空格 ' ' 或者点 '.' 。 
+// 
+//
+// Related Topics 字符串 👍 472 👎 0
+
+package leetcode.editor.cn;
+public class BiaoShiShuZhiDeZiFuChuanLcof{
+    public static void main(String[] args){
+        Solution solution = new BiaoShiShuZhiDeZiFuChuanLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isNumber(String s) {
+            Map<State, Map<CharType, State>> transfer = new HashMap<State, Map<CharType, State>>();
+            Map<CharType, State> initialMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_SPACE, State.STATE_INITIAL);
+                put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+                put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+                put(CharType.CHAR_SIGN, State.STATE_INT_SIGN);
+            }};
+            transfer.put(State.STATE_INITIAL, initialMap);
+            Map<CharType, State> intSignMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+                put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+            }};
+            transfer.put(State.STATE_INT_SIGN, intSignMap);
+            Map<CharType, State> integerMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+                put(CharType.CHAR_EXP, State.STATE_EXP);
+                put(CharType.CHAR_POINT, State.STATE_POINT);
+                put(CharType.CHAR_SPACE, State.STATE_END);
+            }};
+            transfer.put(State.STATE_INTEGER, integerMap);
+            Map<CharType, State> pointMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+                put(CharType.CHAR_EXP, State.STATE_EXP);
+                put(CharType.CHAR_SPACE, State.STATE_END);
+            }};
+            transfer.put(State.STATE_POINT, pointMap);
+            Map<CharType, State> pointWithoutIntMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            }};
+            transfer.put(State.STATE_POINT_WITHOUT_INT, pointWithoutIntMap);
+            Map<CharType, State> fractionMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+                put(CharType.CHAR_EXP, State.STATE_EXP);
+                put(CharType.CHAR_SPACE, State.STATE_END);
+            }};
+            transfer.put(State.STATE_FRACTION, fractionMap);
+            Map<CharType, State> expMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+                put(CharType.CHAR_SIGN, State.STATE_EXP_SIGN);
+            }};
+            transfer.put(State.STATE_EXP, expMap);
+            Map<CharType, State> expSignMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+            }};
+            transfer.put(State.STATE_EXP_SIGN, expSignMap);
+            Map<CharType, State> expNumberMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+                put(CharType.CHAR_SPACE, State.STATE_END);
+            }};
+            transfer.put(State.STATE_EXP_NUMBER, expNumberMap);
+            Map<CharType, State> endMap = new HashMap<CharType, State>() {{
+                put(CharType.CHAR_SPACE, State.STATE_END);
+            }};
+            transfer.put(State.STATE_END, endMap);
+
+            int length = s.length();
+            State state = State.STATE_INITIAL;
+
+            for (int i = 0; i < length; i++) {
+                CharType type = toCharType(s.charAt(i));
+                if (!transfer.get(state).containsKey(type)) {
+                    return false;
+                } else {
+                    state = transfer.get(state).get(type);
+                }
+            }
+            return state == State.STATE_INTEGER || state == State.STATE_POINT || state == State.STATE_FRACTION || state == State.STATE_EXP_NUMBER || state == State.STATE_END;
+        }
+
+        public CharType toCharType(char ch) {
+            if (ch >= '0' && ch <= '9') {
+                return CharType.CHAR_NUMBER;
+            } else if (ch == 'e' || ch == 'E') {
+                return CharType.CHAR_EXP;
+            } else if (ch == '.') {
+                return CharType.CHAR_POINT;
+            } else if (ch == '+' || ch == '-') {
+                return CharType.CHAR_SIGN;
+            } else if (ch == ' ') {
+                return CharType.CHAR_SPACE;
+            } else {
+                return CharType.CHAR_ILLEGAL;
+            }
+        }
+
+        enum State {
+            STATE_INITIAL,
+            STATE_INT_SIGN,
+            STATE_INTEGER,
+            STATE_POINT,
+            STATE_POINT_WITHOUT_INT,
+            STATE_FRACTION,
+            STATE_EXP,
+            STATE_EXP_SIGN,
+            STATE_EXP_NUMBER,
+            STATE_END
+        }
+
+        enum CharType {
+            CHAR_NUMBER,
+            CHAR_EXP,
+            CHAR_POINT,
+            CHAR_SIGN,
+            CHAR_SPACE,
+            CHAR_ILLEGAL
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
