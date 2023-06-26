048f21e59ace8fb747649d9a85b9e3ced662d889
fcoolish <fcoolish@qq.com>
2023-06-26 22:01:12 +0800

    update:lc

---
+++ leetcode/editor/cn/leetcode/editor/cn/Z1R5dt.java
@@ -0,0 +1,88 @@
+//实现一个 MapSum 类，支持两个方法，insert 和 sum： 
+//
+// 
+// MapSum() 初始化 MapSum 对象 
+// void insert(String key, int val) 插入 key-val 键值对，字符串表示键 key ，整数表示值 val 。如果键 
+//key 已经存在，那么原来的键值对将被替代成新的键值对。 
+// int sum(string prefix) 返回所有以该前缀 prefix 开头的键 key 的值的总和。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//inputs = ["MapSum", "insert", "sum", "insert", "sum"]
+//inputs = [[], ["apple", 3], ["ap"], ["app", 2], ["ap"]]
+//输出：
+//[null, null, 3, null, 5]
+//
+//解释：
+//MapSum mapSum = new MapSum();
+//mapSum.insert("apple", 3);  
+//mapSum.sum("ap");           // return 3 (apple = 3)
+//mapSum.insert("app", 2);    
+//mapSum.sum("ap");           // return 5 (apple + app = 3 + 2 = 5)
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= key.length, prefix.length <= 50 
+// key 和 prefix 仅由小写英文字母组成 
+// 1 <= val <= 1000 
+// 最多调用 50 次 insert 和 sum 
+// 
+//
+// 
+//
+// 
+// 注意：本题与主站 677 题相同： https://leetcode-cn.com/problems/map-sum-pairs/ 
+//
+// Related Topics 设计 字典树 哈希表 字符串 👍 28 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Z1R5dt{
+    public static void main(String[] args){
+        //Solution solution = new Z1R5dt().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class MapSum {
+
+    Map<String,Integer> map;
+    /** Initialize your data structure here. */
+    public MapSum() {
+        map = new HashMap<>();
+    }
+    
+    public void insert(String key, int val) {
+        map.put(key,val);
+    }
+    
+    public int sum(String prefix) {
+        int ss = 0;
+        for(Map.Entry<String,Integer> entry:map.entrySet()){
+            if(entry.getKey().startsWith(prefix)){
+                ss+=entry.getValue();
+            }
+        }
+        return ss;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
