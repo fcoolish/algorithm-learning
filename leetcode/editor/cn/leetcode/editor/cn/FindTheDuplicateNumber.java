574ae1fab08ae026c109b7f21f82abf4fd1d131e
fcoolish <fcoolish@qq.com>
2023-07-29 23:29:09 +0800

    update:lc

--- leetcode/editor/cn/leetcode/editor/cn/FindTheDuplicateNumber.java
+++ leetcode/editor/cn/leetcode/editor/cn/FindTheDuplicateNumber.java
@@ -70,7 +70,7 @@ class Solution {
         return -1;
     }
 
-    public int findDuplicate(int[] nums){
+    public int findDuplicate2(int[] nums){
         int n = nums.length;
         int l = 0,r = n - 1;
         int ans = -1;
@@ -91,6 +91,26 @@ class Solution {
             }
         }
         return ans;
+    }
+
+    public int findDuplicate(int[] nums){
+        int n = nums.length;
+        int l = 1,r = n - 1;
+        while (l < r){
+            int mid = l + (r - l)/2;
+            int cnt = 0;
+            for(int i = 0;i < n;i++){
+                if(nums[i] <= mid){
+                    cnt++;
+                }
+            }
+            if(cnt <= mid){
+                l = mid + 1;
+            }else{
+                r = mid;
+            }
+        }
+        return r;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/cn/leetcode/editor/cn/PartitionEqualSubsetSum.java
+++ leetcode/editor/cn/leetcode/editor/cn/PartitionEqualSubsetSum.java
@@ -39,26 +39,25 @@ class Solution {
 
     public boolean canPartition(int[] nums){
         int n = nums.length;
-        if(n < 2)return false;
         int sum = 0;
         int max = nums[0];
         for(int num:nums){
+            sum += num;
             max = Math.max(max,num);
-            sum +=num;
         }
-        if(sum % 2 != 0)return false;
-        int target = sum/2;
-        if(max > target) return false;
+        int target = sum /2;
+        if(max > target || sum % 2 != 0){
+            return false;
+        }
         boolean[][] dp = new boolean[n][target + 1];
         for(int i =0;i < n;i++){
             dp[i][0] = true;
         }
         dp[0][nums[0]] = true;
-        for(int i =1;i < n;i++){
-            int num = nums[i];
-            for(int j = 1;j <= target;j++){
-                if(j >= num){
-                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+        for(int i = 1;i < n;i++){
+            for(int j = 0;j <= target;j++){
+                if(j >= nums[i]){
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - nums[i]];
                 }else{
                     dp[i][j] = dp[i - 1][j];
                 }
@@ -66,6 +65,7 @@ class Solution {
         }
         return dp[n - 1][target];
     }
+
     public boolean canPartition1(int[] nums) {
         int n = nums.length;
         if(n < 2) return false;
