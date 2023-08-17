c89163d1eb95aef1895c8c6a75bf9c1097d224ed
fcoolish <fcoolish@qq.com>
2023-08-17 18:00:11 +0800

    update:lc

--- leetcode/editor/cn/leetcode/editor/cn/StringToIntegerAtoi.java
+++ leetcode/editor/cn/leetcode/editor/cn/StringToIntegerAtoi.java
@@ -88,7 +88,6 @@ public class StringToIntegerAtoi{
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-
         public int myAtoi(String str){
             int n = str.length();
             int idx = 0;
@@ -115,33 +114,6 @@ class Solution {
             }
             return  neg ? -ans:ans;
         }
-        public int myAtoi1(String str) {
-            char[] chars = str.toCharArray();
-            int n = chars.length;
-            int idx = 0;
-            while (idx < n && chars[idx] == ' '){
-                idx++;
-            }
-            if(idx == n)return 0;
-            boolean negative = false;
-            if(chars[idx] == '-'){
-                negative = true;
-                idx++;
-            }else if(chars[idx] == '+'){
-                idx++;
-            }
-            int ans = 0;
-            while (idx < n && Character.isDigit(chars[idx])){
-                int digit = chars[idx] - '0';
-                if(ans > (Integer.MAX_VALUE - digit) / 10){
-                    return negative ? Integer.MIN_VALUE:Integer.MAX_VALUE;
-                }
-                ans = ans * 10 + digit;
-                idx++;
-            }
-            return negative ? -ans:ans;
-        }
-
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
